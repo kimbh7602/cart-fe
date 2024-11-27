f807cdc66e24d069addcf4cad77ab2ee4db90136
김보현 <kimbh7652@gmail.com>
2024-11-27 17:09:53 +0900

    카카오 로그인 롤백 테스트

--- src/components/signIn/LoginComponent.tsx
+++ src/components/signIn/LoginComponent.tsx
@@ -4,42 +4,42 @@ import Image from 'next/image'
 import { isIOS } from 'react-device-detect'
 
 import * as S from './styled'
-import { useAtom } from 'jotai'
-import { isLoadingAtom } from '@/store'
-import { login } from '@/api'
+// import { useAtom } from 'jotai'
+// import { isLoadingAtom } from '@/store'
+// import { login } from '@/api'
 
 const LoginComponent = () => {
-  const [, setIsLoading] = useAtom(isLoadingAtom)
+  // const [, setIsLoading] = useAtom(isLoadingAtom)
 
-  const handleClickKaKao = async () => {
-    console.log('click kakao')
-    setIsLoading(true)
-    try {
-      const { data } = await login({
-        url: `https://ggadam-dev.shop/oauth2/authorization/kakao`,
-      })
+  // const handleClickKaKao = async () => {
+  //   console.log('click kakao')
+  //   setIsLoading(true)
+  //   try {
+  //     const { data } = await login({
+  //       url: `https://ggadam-dev.shop/oauth2/authorization/kakao`,
+  //     })
 
-      console.log(data)
-      setIsLoading(false)
-    } catch (e) {
-      console.log(e)
-      setIsLoading(false)
-    }
-  }
+  //     console.log(data)
+  //     setIsLoading(false)
+  //   } catch (e) {
+  //     console.log(e)
+  //     setIsLoading(false)
+  //   }
+  // }
 
   return (
     <S.Wrapper>
       <S.BottomButtons>
         <h3>3초 만에 시작해 보세요!</h3>
         <S.Buttons>
-          <S.KaKaoButton onClick={handleClickKaKao}>
-            <Image src='/kakao-logo.svg' width='18' height='18' alt='kakao' />
-            <p>카카오 로그인</p>
-          </S.KaKaoButton>
-          {/* <S.KaKaoButton  href='https://ggadam-dev.shop/oauth2/authorization/kakao'>
+          {/* <S.KaKaoButton onClick={handleClickKaKao}>
             <Image src='/kakao-logo.svg' width='18' height='18' alt='kakao' />
             <p>카카오 로그인</p>
           </S.KaKaoButton> */}
+          <S.KaKaoButton href='https://ggadam-dev.shop/oauth2/authorization/kakao'>
+            <Image src='/kakao-logo.svg' width='18' height='18' alt='kakao' />
+            <p>카카오 로그인</p>
+          </S.KaKaoButton>
 
           {isIOS && (
             <S.AppleButton>
